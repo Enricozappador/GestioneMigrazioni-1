a27a3bfb83fbca4c70e00736c69c0e6b1f0e62a9
Enrico Zappador <s236559@studenti.polito.it>
2020-06-18 10:39:08 +0200

    fine es

--- src/main/java/it/polito/tdp/borders/model/Model.java
+++ src/main/java/it/polito/tdp/borders/model/Model.java
@@ -17,9 +17,11 @@ public class Model {
 	
 	private Graph<Country, DefaultEdge> graph ;
 	private Map<Integer,Country> countriesMap ;
+	private Simulatore sim; 
 	
 	public Model() {
 		this.countriesMap = new HashMap<>() ;
+		this.sim = new Simulatore(); 
 
 	}
 	
@@ -42,6 +44,13 @@ public class Model {
 		}
 	}
 	
+	public List<Country> getCountries() {
+		List<Country> countries = new ArrayList<Country>(); 
+		countries.addAll(this.graph.vertexSet()); 
+		Collections.sort(countries);
+		return countries;
+	} 
+	
 	public List<CountryAndNumber> getCountryAndNumber() {
 		List<CountryAndNumber> list = new ArrayList<>() ;
 		
@@ -51,5 +60,32 @@ public class Model {
 		Collections.sort(list);
 		return list ;
 	}
+	
+	public void simula(Country partenza) {
+		if(this.graph!=null) {
+		sim.init(partenza,this.graph); 
+		sim.run();
+		
+		}
+	}
+	
+	public Integer getTime() {
+		return this.sim.getT(); 
+	}
+	
+	public List<CountryAndNumber> getStanziali(){
+		Map<Country, Integer> stanziali = this.sim.getStanziali(); 
+		List<CountryAndNumber> res = new ArrayList<>(); 
+		
+		for(Country c : stanziali.keySet()) {
+			CountryAndNumber cn =  new CountryAndNumber(c, stanziali.get(c)); 
+			res.add(cn); 
+		}
+		Collections.sort(res);
+		return res; 
+	}
+
+	
+	
 
 }
--- src/main/java/it/polito/tdp/borders/model/Simulatore.java
+++ src/main/java/it/polito/tdp/borders/model/Simulatore.java
@@ -1,8 +1,12 @@
 package it.polito.tdp.borders.model;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 
 public class Simulatore {
@@ -12,11 +16,64 @@ public class Simulatore {
 	private int N_MIGRANTI = 1000; 
 	private Country partenza; 
 	
-	private int T; 
+	private int T =-1; 
 	private Map<Country, Integer> stanziali; 
 	
 	private PriorityQueue<Evento> queue; 
 	
+	public void init(Country partenza, Graph<Country, DefaultEdge> grafo) {
+		this.partenza = partenza; 
+		this.grafo = grafo; 
+		
+		this.T = 1; 
+		
+		stanziali = new HashMap<>(); 
+		
+		for(Country c : this.grafo.vertexSet()) {
+			stanziali.put(c, 0); 
+			
+		}
+		
+		this.queue = new PriorityQueue<Evento>(); 
+		
+		this.queue.add(new Evento(T, partenza, N_MIGRANTI)); 
+	}
+	
+	public void run() {
+		
+		Evento e; 
+		while((e = this.queue.poll()) != null){
+			this.T = e.getT(); 
+			int nPersone =  e.getN(); 
+			Country stato = e.getStato(); 
+			
+			
+			List<Country> vicini = Graphs.neighborListOf(this.grafo, stato); 
+			
+			int migranti = (nPersone/2) / vicini.size(); 
+			
+			if(migranti > 0) {
+			
+				for(Country confinante : vicini) {
+					queue.add(new Evento(e.getT()+1, confinante, migranti)); 
+					
+				}
+			}
+			
+			int stanziali = nPersone - migranti*vicini.size();
+			this.stanziali.put(stato, this.stanziali.get(stato)+ stanziali); 
+			
+			
+		}
+	}
+	
+	public Map<Country, Integer> getStanziali(){
+		return this.stanziali; 
+	}
+	
+	public Integer getT() {
+		return this.T; 
+	}
 	
 	
 
